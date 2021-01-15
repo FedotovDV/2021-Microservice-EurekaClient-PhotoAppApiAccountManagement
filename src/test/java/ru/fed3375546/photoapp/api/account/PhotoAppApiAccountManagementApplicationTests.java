683c570183ec407039f586b60826ca3d4e54162e
Dmitriy <fed3375546@yandex.ru>
2021-01-15 13:14:53 +0300

    Initial commit

---
+++ src/test/java/ru/fed3375546/photoapp/api/account/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fed3375546.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
